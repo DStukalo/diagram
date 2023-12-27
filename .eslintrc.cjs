0da3dba432899ba9e4c3545fbd75d3baeb819b9d
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-12-27 15:02:59 +0200

    init: Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,99 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    './.eslintrc-typescript.cjs',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'trailing-comma': 0,
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    indent: [2, 'tab'],
+    'react/jsx-indent': [
+      2,
+      'tab',
+      { checkAttributes: false, indentLogicalExpressions: true },
+    ],
+    'react/jsx-indent-props': [2, 'tab'],
+    'no-tabs': 0,
+    'linebreak-style': [2, 'unix'],
+    quotes: [2, 'single'],
+    semi: [2, 'always'],
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/require-default-props': ['off'],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+      },
+    ],
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/no-unknown-property': [2, { ignore: ['css'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state'],
+      },
+    ],
+  },
+};
